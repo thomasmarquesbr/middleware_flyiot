ef8aeade6178624de309843a33893a5d5de2f28f
Thomas <msamoht16@gmail.com>
2018-11-25 21:24:32 -0200

    Atualiza docker-compose
    Adiciona data_management_service ao docker-compose

---
+++ discovery.py
@@ -0,0 +1,51 @@
+import logging
+import socket
+import sys
+import const
+from time import sleep
+from typing import cast
+
+from zeroconf import ServiceBrowser, ServiceStateChange, Zeroconf
+
+def on_service_state_change(
+    zeroconf: Zeroconf, service_type: str, name: str, state_change: ServiceStateChange,
+) -> None:
+    if const.MIDDLEWARE_NAME in name:
+        print("Service %s of type %s state changed: %s" % (name, service_type, state_change))
+
+        if state_change is ServiceStateChange.Added:
+            info = zeroconf.get_service_info(service_type, name)
+            if info:
+                print("  Address: %s:%d" % (socket.inet_ntoa(cast(bytes, info.address)), cast(int, info.port)))
+                # print("  Weight: %d, priority: %d" % (info.weight, info.priority))
+                # print("  Server: %s" % (info.server,))
+                if info.properties:
+                    print("  Properties are: ")
+                    print(info.properties.items)
+                    for key, value in info.properties.items():
+                        print("    %s: %s" % (key, value))
+                else:
+                    print("  No properties")
+            else:
+                print("  No info")
+            print('\n')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    if len(sys.argv) > 1:
+        assert sys.argv[1:] == ['--debug']
+        logging.getLogger('zeroconf').setLevel(logging.DEBUG)
+
+    zeroconf = Zeroconf()
+    print("\nBrowsing services, press Ctrl-C to exit...\n")
+    browser = ServiceBrowser(zeroconf, "_http._tcp.local.", handlers=[on_service_state_change])
+
+    try:
+        while True:
+            sleep(0.1)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        zeroconf.close()
+
--- start.py
+++ start.py
@@ -1,3 +1,4 @@
 import os
 
-os.system("python3 register.py")
+os.system("python3 register.py &")
+os.system("python3 discovery.py")
